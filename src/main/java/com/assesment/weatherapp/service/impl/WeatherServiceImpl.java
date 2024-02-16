ae445ebad8c3fe2e953283d8e2bb6b00cf00d371
Akshay Kisan Pokharkar <87112477+akshaypokharkar11@users.noreply.github.com>
2024-02-16 14:17:20 +0530

    Optimize imports

--- src/main/java/com/assesment/weatherapp/service/impl/WeatherServiceImpl.java
+++ src/main/java/com/assesment/weatherapp/service/impl/WeatherServiceImpl.java
@@ -2,20 +2,10 @@ package com.assesment.weatherapp.service.impl;
 
 import com.assesment.weatherapp.adapter.ForecastAdapter;
 import com.assesment.weatherapp.dto.ForecastResponse;
-import com.assesment.weatherapp.exception.ForecastNotFoundException;
 import com.assesment.weatherapp.exception.ForecastServiceException;
 import com.assesment.weatherapp.service.WeatherService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
-import org.springframework.web.client.HttpServerErrorException;
-import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class WeatherServiceImpl implements WeatherService {
